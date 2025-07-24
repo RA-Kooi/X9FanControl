08a20d6f227473872c7f21a16f6ba48037b4d522
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2025-07-25 01:45:25 +0200

    Properly use the application lifetime interface

--- X9FanControl/Lifetime.cs
+++ X9FanControl/Lifetime.cs
@@ -9,40 +9,43 @@ using Microsoft.Extensions.Logging;
 
 class Lifetime: IHostedService
 {
-	private bool stop;
 	private readonly ILogger<Lifetime> log;
 	private readonly IHostApplicationLifetime appLifetime;
-	private Task? mainLoop;
 
 	public Lifetime(
 		ILogger<Lifetime> logger,
 		IHostApplicationLifetime appLifetime)
 	{
-		this.stop = false;
 		log = logger;
 		this.appLifetime = appLifetime;
-		this.mainLoop = null;
 	}
 
 	public Task StartAsync(CancellationToken _)
 	{
-		mainLoop = Task.Run(async() =>
+		Action<Task> OnError = (_) =>
 		{
-			while(!stop)
-			{
-				log.LogInformation("Sleeping...");
-				await Task.Delay(5000);
-			}
+			appLifetime.StopApplication();
+		};
+
+		appLifetime.ApplicationStarted.Register(() =>
+		{
+
+			log.LogInformation("Started");
+		});
+
+		appLifetime.ApplicationStopping.Register(() =>
+		{
+			log.LogInformation("Exiting...");
+
+		});
+
+		appLifetime.ApplicationStopped.Register(() =>
+		{
+			log.LogInformation("Done..");
 		});
 
 		return Task.CompletedTask;
 	}
 
-	public async Task StopAsync(CancellationToken _)
-	{
-		log.LogInformation("Exiting...");
-
-		stop = true;
-		await mainLoop!;
-	}
+	public Task StopAsync(CancellationToken _) => Task.CompletedTask;
 }
