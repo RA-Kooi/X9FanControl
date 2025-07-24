b7566e40b7b91aa991079b1af8eed48c80a9209f
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2025-07-24 04:29:00 +0200

    Find required tools

--- X9FanControl/Main.cs
+++ X9FanControl/Main.cs
@@ -1,9 +1,56 @@
 namespace X9FanControl;
 
+using System;
+using System.IO;
+
 class Program
 {
-	static void Main(string[] args)
+	static string? FindProgram(string[] paths, string program)
 	{
-		Console.WriteLine("Hello, World!");
+		foreach(string path in paths)
+		{
+			string fullPath = Path.Join(path, program);
+			if(File.Exists(fullPath))
+				return fullPath;
+		}
+		return null;
+	}
+
+	static int Main(string[] args)
+	{
+		string path = Environment.GetEnvironmentVariable("PATH")!;
+		string[] paths = path.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+		string? lsScsi   = FindProgram(paths, "lsscsi");
+		string? hddTemp  = FindProgram(paths, "hddtemp");
+		string? lsiUtil  = FindProgram(paths, "lsiutil");
+		string? ipmiTool = FindProgram(paths, "ipmitool");
+
+		bool error = false;
+		if(lsScsi == null)
+		{
+			Console.Error.WriteLine("Unable to find lsscsi!");
+			error = true;
+		}
+		if(hddTemp == null)
+		{
+			Console.Error.WriteLine("Unable to find hddtemp");
+			error = true;
+		}
+		if(lsiUtil == null)
+		{
+			Console.Error.WriteLine("Unable to find lsiutil");
+			error = true;
+		}
+		if(ipmiTool == null)
+		{
+			Console.Error.WriteLine("Unable to find ipmitool");
+			error = true;
+		}
+
+		if(error)
+			return 1;
+
+		return 0;
 	}
 }
